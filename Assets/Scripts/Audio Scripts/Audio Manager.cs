302bad33bee5a7cb981b75d9d47f3a0389923886
althom12 <109119094+althom12@users.noreply.github.com>
2025-03-25 22:53:22 +0000

    Added Audio SFX for characters
    Reaper audio

--- Assets/Scripts/Audio Scripts/Audio Manager.cs
+++ Assets/Scripts/Audio Scripts/Audio Manager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    
+    public static AudioManager instance { get; private set;}
+
 
     
     void Start()
